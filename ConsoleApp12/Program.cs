4677998c38e67dac4f69b9f1150607f707dedc56
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-11 17:02:04 +0530

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        
+        private static void LINQReadXML()
+        {
+            string myXML = @"<Departments>
+                           <Department>Account</Department>
+                           <Department>Sales</Department>
+                           <Department>Pre-Sales</Department>
+                           <Department>Marketing</Department>
+                           </Department>";
+
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Parse(myXML);
+            var result = xdoc.Element("Departments").Descendants();
+            foreach (XElement item in result)
+            {
+                Console.WriteLine("Department Name - " + item.Value);
+            }
+            Console.WriteLine("\npress any key to continue.");
+            Console.ReadLine();
+            
+        }
+    }
+}
